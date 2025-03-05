712126b377e752fef758112ece8bf1d10eeeca88
Eno-Inyang <ibukuninyang@gmail.com>
2025-03-05 16:15:19 +0100

    update requirements.txt

--- app/constants.py
+++ app/constants.py
@@ -3,6 +3,7 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL')
 GROQ_API_KEY = os.getenv('GROQ_API_KEY')
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+
 #Flask Config
 APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
