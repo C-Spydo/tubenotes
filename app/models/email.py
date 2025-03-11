579dd2e603dd64781d2cf5da5ad6828868b55923
Eno-Inyang <ibukuninyang@gmail.com>
2025-03-11 07:46:59 +0100

    clean up models

---
+++ app/models/email.py
@@ -0,0 +1,17 @@
+from ..extensions.database import database
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+class Email(database.Model):
+    __tablename__ = 'emails'
+
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    prospect_id = database.Column(database.Integer, database.ForeignKey('prospects.id'), nullable=False)
+    message = database.Column(database.Text, nullable=False)
+    created_at = database.Column(database.DateTime(timezone=True), server_default=func.now())
+    updated_at = database.Column(database.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    prospect = relationship("Prospect", back_populates="emails")
+
+    def __repr__(self):
+        return f'<Email {self.id}>'
---
+++ app/models/prospect.py
@@ -0,0 +1,24 @@
+from app.extensions import database, session
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
+class Prospect(database.Model):
+    __tablename__ = 'prospects'
+
+    id = database.Column(database.Integer, primary_key=True)
+    industry_id = database.Column(database.Integer, nullable=False)
+    company_name = database.Column(database.String(100), nullable=False) 
+    contact_name = database.Column(database.String(100), nullable=False)
+    contact_email = database.Column(database.String(100), nullable=False)
+    contact_phone = database.Column(database.String(100), nullable=False)
+    created_at = database.Column(database.DateTime(timezone=True), server_default=func.now())
+    updated_at = database.Column(database.DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    emails = relationship("Email", back_populates="prospect") 
+
+    def __repr__(self):
+        return f'<Prospect {self.id}>'
+
+    
+  
