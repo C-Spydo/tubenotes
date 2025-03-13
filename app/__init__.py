7fe38a0e9d2cf807d31b9f54527da1e0aa19ca0d
Eno-Inyang <ibukuninyang@gmail.com>
2025-03-13 18:50:52 +0100

    edit template

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ from flask import Flask
 load_dotenv()
 
 def create_app(config_class=Config):
-    app = Flask(__name__, static_folder='app/static')
+    app = Flask(__name__)
 
     app.config.from_object(config_class)
 
