8678da471736970f086b3b10796eb8f83c8312c7
Samson K Okemakinde <csamsonok@gmail.com>
2025-03-13 11:21:45 +0100

    Update email.py

--- app/controllers/email.py
+++ app/controllers/email.py
@@ -3,7 +3,7 @@ from app.repository.base import get_record_by_field
 from app.services.cold_mail import generate_cold_mail
 from . import routes_blueprint
 from ..error_handler import url_validation_error_handler
-from flask_parameter_validation import ValidateParameters, Route
+from flask_parameter_validation import ValidateParameters, Route, Json
 from ..helpers import create_response
 from ..constants import SUCCESS_MESSAGE,DUMMY_EMAIL
 from ..enums import CustomStatusCode, EmailStatus
@@ -21,7 +21,9 @@ def get_cold_emails():
         "id": 1,
         "status": EmailStatus.SENT.value,
         "contact_name": "Lang Chain",
-        "contact_email": "langchain@mail.com",   
+        "contact_email": "langchain@mail.com",
+        "company_name": "The Company Ltd",
+        "title": "Here is the title of the Mail",
         "message": DUMMY_EMAIL,
         "created_at":  datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
         "updated_at":  datetime.today().strftime("%Y-%m-%d %H:%M:%S")
@@ -30,7 +32,9 @@ def get_cold_emails():
         "id": 2,
         "status": EmailStatus.SENT.value,
         "contact_name": "Dina Mitrov",
-        "contact_email": "dinamitrov@mail.com",  
+        "contact_email": "dinamitrov@mail.com",
+        "company_name": "The Company Ltd",
+        "title": "Here is the title of the Mail",
         "message": DUMMY_EMAIL,
         "created_at":  datetime.today().strftime("%Y-%m-%d %H:%M:%S"),
         "updated_at":  datetime.today().strftime("%Y-%m-%d %H:%M:%S")
@@ -43,3 +47,8 @@ def get_cold_emails():
 def get_email(id:int = Route()):
     email = get_record_by_field(Email, 'id', id).serialize()
     return create_response(CustomStatusCode.SUCCESS.value, SUCCESS_MESSAGE, email), 200
+
+@routes_blueprint.route('/emails/send', methods=['POST'])
+@ValidateParameters(url_validation_error_handler)
+def send_email(prospect_id:int = Json(), message:str = Json()):
+    return create_response(CustomStatusCode.SUCCESS.value, SUCCESS_MESSAGE, prospect), 201
