5e9c7c3f079bcbfe412108387665a06980a01752
Eno-Inyang <ibukuninyang@gmail.com>
2025-03-20 21:59:15 +0100

    add delete and get notebook endpoint

--- app/controllers/notebook.py
+++ app/controllers/notebook.py
@@ -16,6 +16,18 @@ def get_note_from_query(user_id:int = Query(), query:str = Query()):
 
 @routes_blueprint.route('/notes/<int:id>', methods=['GET'])
 @ValidateParameters(url_validation_error_handler)
-def get_user_notes(id:int = Route()):
-    notebooks = notebook.get_user_notebooks(id)
+def get_note(id:int = Route()):
+    notebook = base.get_record_by_field(Notebook, "id", id)
+    return create_response(CustomStatusCode.SUCCESS.value, SUCCESS_MESSAGE, notebook.serialize()), 200
+
+@routes_blueprint.route('/users/<int:user_id>/notes', methods=['GET'])
+@ValidateParameters(url_validation_error_handler)
+def get_user_notes(user_id:int = Route()):
+    notebooks = notebook.get_user_notebooks(user_id)
     return create_response(CustomStatusCode.SUCCESS.value, SUCCESS_MESSAGE, notebooks), 200
+
+@routes_blueprint.route('/notes/<int:id>', methods=['DELETE'])
+@ValidateParameters(url_validation_error_handler)
+def delete_note(id:int = Route()):
+    base.delete_records_by_field(Notebook, 'id', id)
+    return create_response(CustomStatusCode.SUCCESS.value, SUCCESS_MESSAGE), 200
